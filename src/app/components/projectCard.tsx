9958382c7a989c5ba7cfa07a3ca12ecba87e2275
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2024-08-28 17:05:36 +0700

    28/08/2024

--- src/app/components/projectCard.tsx
+++ src/app/components/projectCard.tsx
@@ -11,15 +11,15 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl, projectLink }) => {
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <Image className="w-full" src={imageUrl} alt={title} width={500} height={300} />
-        <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{title}</div>
-            <p className="text-gray-700 text-base">{description}</p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-            <a href={projectLink} className="bg-blue-500 text-white px-4 py-2 rounded-full">View Project</a>
-        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg p-8 bg-red-200 m-8">
+            <Image className="w-full" src={imageUrl} alt={title} width={500} height={300} />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{title}</div>
+                <p className="text-base">{description}</p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <a href={projectLink} className="bg-blue-500 text-white px-4 py-2 rounded-full">View Project</a>
+            </div>
         </div>
     );
 };
